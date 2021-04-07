341b081f42379f2bb0b4aa635bdf5b87a3307546
Douglas Carvalho <douglasc.dev@gmail.com>
2021-04-07 14:21:07 -0300

    Created setup for pypi

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages
+from setuptools import setup
+
+# reading long description from file
+with open("README.md", encoding="utf-8") as file:
+    long_description = file.read()
+
+
+with open("requirements/requirements.txt", encoding="utf-8") as file:
+    REQUIREMENTS = file.read()
+
+
+# calling the setup function
+setup(
+    name="gmail_attachment_downloader",
+    version="0.0.1",
+    description="A simple snake game",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/douglascdev/gmail_attachment_downloader",
+    author="Douglas",
+    author_email="douglasc.dev@gmail.com",
+    license="MIT License",
+    packages=find_packages(include=["gmail_attachment_downloader"]),
+    entry_points={
+        "console_scripts": [
+            "gmail_attachment_dl = gmail_attachment_downloader.__main__:main",
+        ],
+    },
+    install_requires=REQUIREMENTS,
+    keywords=("email", "gmail", "attachment", "download"),
+)
